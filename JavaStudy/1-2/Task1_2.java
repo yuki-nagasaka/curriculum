325a40c23eec6b6e897c7717b3aa808fbdc45160
yuki-nagasaka <72226286+yuki-nagasaka@users.noreply.github.com>
2020-11-13 16:31:05 +0900

    課題提出
    Task1-2課題

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,22 @@
+public class Task1_2 {
+    public static void main(String[] args) {
+
+    String country[] = {"JAPAN","AMERICA","KOREA"};
+
+    System.out.println(country.length);
+
+    String[] strArray = {"りんご","もも","ぶどう"};
+
+    System.out.println(strArray[1]);
+
+
+    //["10、20、30、40、50を要素の値（初期値）とする、要素数5のintArrayという名前のint型配列を宣言"]
+
+    int[] intArray = { 10, 20, 30, 40, 50 };
+
+
+   // ["2番目の要素と5番目の要素の合計点を求める"]
+    System.out.println(intArray[1] + intArray[4]);
+    
+    }
+}
