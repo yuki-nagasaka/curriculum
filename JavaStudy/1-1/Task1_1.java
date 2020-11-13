acc9d8b99869550f766d6b0fba4d085e40156127
yuki-nagasaka <72226286+yuki-nagasaka@users.noreply.github.com>
2020-11-13 16:22:50 +0900

    課題提出
    Task1_1課題

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,23 @@
+public class Task1_1 {
+public static void main(String[] args) {
+
+int hogeInt = 50;
+
+String hogeString = "テスト";
+
+//["浮動小数点型という名前の変数を宣言"]
+double hogeDouble = 0.2;
+
+hogeInt = 100;
+
+hogeString = "合格";
+
+System.out.println(hogeInt);
+//["hogeStringを表示する"]
+System.out.println(hogeString);
+
+System.out.println(hogeDouble);
+
+
+    }
+}
