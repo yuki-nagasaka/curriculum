d3948929077c431e4c08927663b738daf76dca37
yuki-nagasaka <72226286+yuki-nagasaka@users.noreply.github.com>
2020-11-13 16:31:39 +0900

    課題提出
    Task1-3

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,28 @@
+public class Task1_3{
+public static void main(String[] args) {
+
+int plus = 5 + 10;
+
+int minus = 20 - 7;
+
+
+//["10と2を乗算した値を代入する、変数multiplyを作成"]
+int multiply = 10 * 2;
+
+int remi =  multiply %= 6;
+
+//["townという変数を有楽町で上書き、lineという変数を線で上書き"]
+
+String town = "有楽町";
+String line = "線";
+
+String train = town + line;
+
+System.out.println(plus);
+System.out.println(minus);
+System.out.println(multiply);
+System.out.println(remi);
+System.out.println(train);
+
+}
+}
