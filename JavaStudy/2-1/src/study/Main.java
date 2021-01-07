938b5f265e0d7288e33a4f36c8a40adbee143078
yuki-nagasaka <72226286+yuki-nagasaka@users.noreply.github.com>
2021-01-07 23:26:00 +0900

    Create Main.java
    課題2−1の作成

---
+++ JavaStudy/2-1/src/study/Main.java
@@ -0,0 +1,53 @@
+package study;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//① firstNameとlastNameという名前の変数を定義し、
+	      //   firstNameには自分の名前、lastNameには自分の名字で初期化しなさい
+
+	String firstName = "nagasaka" ;
+	String lastName = "yuki" ;
+
+
+	        //getName関数の呼び出しと出力
+	        System.out.println(getName(firstName, lastName));
+	        System.out.println();
+
+	        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	        //③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
+	        //   要素が奇数の場合には「nは奇数です。」と出力されます。
+	        
+	    	for (int n = 0; n < arr.length; n++) { 
+	    		if(n % 2 != 0) {
+	    		System.out.println(n + "は奇数です");
+	    		}
+	    	}
+	}
+
+
+
+
+	    //② 関数を定義しなさい。
+	    //   関数名：getName
+	    //  修飾子：public
+	    //   引数には①で定義したfirstNameとlastNameを引数で受け取り、
+	    //  last_name と first_name を連結した値を返り値とする変数を定義しなさい。
+	        public static String getName(String last_name, String first_name){
+	        	String fullName = last_name + first_name;
+	        	return fullName;
+		    }
+	    //奇数の判定を行う。
+	    public static void isOdd(int num) {
+	      if(num % 2 != 0) {
+	        System.out.println(num + "は奇数です。");
+	        }
+	  }
+
+
+
+
+}
