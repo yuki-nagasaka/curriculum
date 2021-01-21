42a9287debf78866cd5df0dcd705ee8a6822a165
yuki-nagasaka <72226286+yuki-nagasaka@users.noreply.github.com>
2021-01-21 14:00:17 +0900

    Create Study.java
    2-2課題提出

---
+++ JavaStudy/2−2/src/study/Study.java
@@ -0,0 +1,31 @@
+package study;
+
+/**
+ * Study.java
+ */
+class Account{
+	
+	// フィールド変数
+    private String userName;
+    private int id;
+    private String password;
+
+    // コンストラクタ
+    public Account(String userName, int id, String password) {
+        this.userName = userName;
+        this.id = id;
+        this.password = password;
+    }
+
+    // ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
+    // アクセス修飾子:「protected」
+    // メソッド名:「printAccountInfo」
+    
+    
+    
+    protected void printAccountInfo() {
+    	System.out.println("ユーザー名は、" + userName);
+    	System.out.println("IDは、" + id);
+    	System.out.println("パスワードは、" + password);
+    }
+}
