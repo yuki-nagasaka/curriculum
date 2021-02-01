8c77d76dd913f3f1c096188e78b4afbb5e580fee
yuki-nagasaka <72226286+yuki-nagasaka@users.noreply.github.com>
2021-02-01 21:31:03 +0900

    Create Study.java
    2-4課題

---
+++ JavaStudy/2-4/src/study/Study.java
@@ -0,0 +1,30 @@
+package study;
+
+//①FruitsConstants.javaをインポートしなさい。
+
+	import constants.FruitsConstants;
+
+//②Fruits.javaをインポートしなさい。
+
+	import fruits.Fruits;
+
+	/**
+	* 本課題では、基本的なJavaの構造と、importについて理解を深めていきましょう。
+	*
+	* 問①〜問③まであります。
+	* 全て回答し、課題にある画像と同じ表示になるようにしてください。
+	*
+	*/
+
+	public class Study {
+
+		public static void main(String[] args) {
+			// TODO 自動生成されたメソッド・スタブ
+
+	        /* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
+	         *     また、課題の画像と同じ表示になるように
+	         *     printFruitsの引数にはFruitsConstantsから呼び出した値を設定してください。
+	         */
+		Fruits.printFruits(FruitsConstants.FRUITS_LEMON_04,FruitsConstants.FRUITS_COUNT_20);
+	    }
+    }
