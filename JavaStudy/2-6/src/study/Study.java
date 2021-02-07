c664f35eb55a1b84a0d70452212f99540768e161
yuki-nagasaka <72226286+yuki-nagasaka@users.noreply.github.com>
2021-02-08 00:27:43 +0900

    Update Study.java
    2-6

--- JavaStudy/2-6/src/study/Study.java
+++ JavaStudy/2-6/src/study/Study.java
@@ -38,10 +38,12 @@ public class Study {
         // - この課題に関しては課題実施日によって結果が異なりますので、Wiki課題の画像とは結果が異なります。
         // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
 
-        System.out.println(calendar.get(Calendar.YEAR)+ 1);
+        calendar.add(Calendar.YEAR, 1);
+        System.out.println(calendar.get(Calendar.YEAR));
         calendar.add(Calendar.MONTH, 3);
         System.out.println(calendar.get(Calendar.MONTH));
-        System.out.println(calendar.get(Calendar.DATE)+ 15);
+        calendar.add(Calendar.DATE, 15);
+        System.out.println(calendar.get(Calendar.DATE));
 
 
 
