ac2717f64991632638034526e009ad1e363ea95d
yuki-nagasaka <72226286+yuki-nagasaka@users.noreply.github.com>
2021-02-09 20:57:23 +0900

    Update Study.java
    2-6

--- JavaStudy/2-6/src/study/Study.java
+++ JavaStudy/2-6/src/study/Study.java
@@ -39,11 +39,11 @@ public class Study {
         // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
 
         calendar.add(Calendar.YEAR, 1);
-        calendar.add(Calendar.MONTH, 3);
+        calendar.add(Calendar.MONTH, 2);
         calendar.add(Calendar.DATE, 15);
 
         System.out.println(calendar.get(Calendar.YEAR));
-        System.out.println(calendar.get(Calendar.MONTH));
+        System.out.println(calendar.get(Calendar.MONTH)+1);
         System.out.println(calendar.get(Calendar.DATE));
 
         // うるう年判定
